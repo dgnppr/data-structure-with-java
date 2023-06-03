97bba516815e6fe8aa6708298ac6278af014cf17
dragonappear <dragonappear1@gmail.com>
2023-06-03 23:05:55 +0900

    이중 원형 연결 리스트 구현 : 삽입, 삭제

--- src/main/java/linkedlist/BiNode.java
+++ src/main/java/linkedlist/BiNode.java
@@ -5,14 +5,19 @@ public class BiNode<E> extends Node<E> {
     public BiNode<E> nxt;
     public BiNode<E> prev;
 
+    public BiNode() {
+        super(null);
+        this.nxt = this.prev = this;
+    }
+
     public BiNode(E data, BiNode<E> nxt, BiNode<E> prev) {
         super(data);
         this.nxt = nxt;
         this.prev = prev;
     }
-    
+
     public BiNode(E data) {
         super(data);
-        this.prev = null;
+        this.nxt = this.prev = null;
     }
 }
--- src/test/java/linkedlist/circular/CircularDoublyLinkedListTest.java
+++ src/test/java/linkedlist/circular/CircularDoublyLinkedListTest.java
@@ -1,7 +1,10 @@
 package linkedlist.circular;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 class CircularDoublyLinkedListTest {
     CircularDoublyLinkedList<Integer> linkedList;
@@ -9,6 +12,19 @@ class CircularDoublyLinkedListTest {
     @BeforeEach
     void setUp() {
         linkedList = new CircularDoublyLinkedList<>();
+    }
+
+    private void init() {
+        linkedList.addAtLast(1);
+        linkedList.addAtLast(2);
+        linkedList.addAtLast(3);
+        linkedList.addAtLast(4);
+        linkedList.addAtLast(5);
+        linkedList.addAtLast(6);
+        linkedList.addAtLast(7);
+        linkedList.addAtLast(8);
+        linkedList.addAtLast(9);
+        linkedList.addAtLast(10);
     }
 
     @Test
@@ -23,5 +39,96 @@ class CircularDoublyLinkedListTest {
         linkedList.addAtFirst(3);
         linkedList.addAtFirst(2);
         linkedList.addAtFirst(1);
+
+        List<Integer> integers = linkedList.getAll();
+        Assertions.assertThat(integers).hasSize(10);
+        for (int i = 0; i < integers.size(); i++) {
+            Assertions.assertThat(integers.get(i)).isEqualTo(i + 1);
+        }
+    }
+
+    @Test
+    void addAtLast() {
+        init();
+
+        List<Integer> integers = linkedList.getAll();
+        Assertions.assertThat(integers).hasSize(10);
+        for (int i = 0; i < integers.size(); i++) {
+            Assertions.assertThat(integers.get(i)).isEqualTo(i + 1);
+        }
+    }
+
+    @Test
+    void addAt() {
+        linkedList.addAtLast(1);
+        linkedList.addAtLast(2);
+        linkedList.addAtLast(3);
+        linkedList.addAtLast(4);
+        linkedList.addAtLast(6);
+        linkedList.addAtLast(7);
+        linkedList.addAtLast(8);
+        linkedList.addAtLast(9);
+        linkedList.addAtLast(10);
+
+        linkedList.addAt(5, 4);
+
+        List<Integer> integers = linkedList.getAll();
+        Assertions.assertThat(integers).hasSize(10);
+        for (int i = 0; i < integers.size(); i++) {
+            Assertions.assertThat(integers.get(i)).isEqualTo(i + 1);
+        }
+    }
+
+    @Test
+    void removeFirst() {
+        linkedList.addAtLast(1);
+        linkedList.addAtLast(2);
+        linkedList.addAtLast(3);
+
+        linkedList.removeFirst();
+        linkedList.removeFirst();
+
+        List<Integer> integers = linkedList.getAll();
+        Assertions.assertThat(integers).hasSize(1);
+        for (int i = 0; i < integers.size(); i++) {
+            Assertions.assertThat(integers.get(i)).isEqualTo(i + 3);
+        }
+    }
+
+    @Test
+    void removeLast() {
+        init();
+
+        linkedList.removeLast();
+        linkedList.removeLast();
+        linkedList.removeLast();
+
+        List<Integer> integers = linkedList.getAll();
+        Assertions.assertThat(integers).hasSize(7);
+        for (int i = 0; i < integers.size(); i++) {
+            Assertions.assertThat(integers.get(i)).isEqualTo(i + 1);
+        }
+    }
+
+    @Test
+    void remove() {
+        init();
+
+        linkedList.remove(9);
+        linkedList.remove(8);
+        linkedList.remove(0);
+
+        List<Integer> integers = linkedList.getAll();
+        Assertions.assertThat(integers).hasSize(7);
+        Assertions.assertThat(integers).doesNotContain(1, 9, 10);
+    }
+
+    @Test
+    void clear() {
+        init();
+        linkedList.clear();
+
+        List<Integer> integers = linkedList.getAll();
+        Assertions.assertThat(integers).isEmpty();
     }
 }

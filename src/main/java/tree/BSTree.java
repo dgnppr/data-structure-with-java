c4d1534b32afcd87304d4512a574ef810124df05
dragonappear <dragonappear1@gmail.com>
2023-06-09 23:20:11 +0900

    Add: Binary Search Tree
    - add: 삽입 메소드

--- src/main/java/tree/BSTree.java
+++ src/main/java/tree/BSTree.java
@@ -18,12 +18,33 @@ public class BSTree<K, V> {
         while (true) {
             if (ptr == null) return null;
 
-            int comp = compare(key, ptr.getKey());
+            int comp = compare(key, ptr.key);
 
-            if (comp == 0) return ptr.getValue();
-            else if (comp < 0) ptr = ptr.getLeft();
-            else ptr = ptr.getRight();
+            if (comp == 0) return ptr.value;
+            else if (comp < 0) ptr = ptr.left;
+            else ptr = ptr.right;
         }
+    }
+
+    public void add(K key, V data) {
+        if (this.root == null)
+            root = new Node<>(key, data, null, null);
+        else
+            addNode(root, key, data);
+    }
+
+    private void addNode(Node<K, V> node, K key, V data) {
+        int comp = compare(key, node.getKey());
+        if (comp == 0) return;
+
+        if (comp < 0) {
+            if (node.left == null) node.left = new Node<>(key, data, null, null);
+            else addNode(node.left, key, data);
+        } else {
+            if (node.right == null) node.right = new Node<>(key, data, null, null);
+            else addNode(node.right, key, data);
+        }
+
     }
 
     private int compare(K k1, K k2) {
--- src/main/java/tree/Node.java
+++ src/main/java/tree/Node.java
@@ -5,10 +5,10 @@ import lombok.Getter;
 @Getter
 public class Node<K, V> {
 
-    private K key;
-    private V value;
-    private Node<K, V> left;
-    private Node<K, V> right;
+    public K key;
+    public V value;
+    public Node<K, V> left;
+    public Node<K, V> right;
 
     public Node(K key, V value, Node<K, V> left, Node<K, V> right) {
         this.key = key;
